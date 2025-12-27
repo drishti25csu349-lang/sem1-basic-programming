be73b3564708b8fb1bd628f02609174432213a93
Drishti Saini <drishti25csu349@ncuindia.edu>
2025-12-27 20:36:28 +0530

    Create conditionandloops2.c

---
+++ conditionandloops2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int num;
+  printf("Enter the number\n");
+  scanf("%d",&num);
+  for (int j=2;j<num;j++){
+    if(num%j==0){
+      printf("the number is not prime.");
+    
+    return 0;}
