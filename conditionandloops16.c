f070170b61fba9e622b926e835efd18d2622ada1
Drishti Saini <drishti25csu349@ncuindia.edu>
2025-12-27 22:31:39 +0530

    Create conditionandloops16.c

---
+++ conditionandloops16.c
@@ -0,0 +1,7 @@
+  int x,y, result;
+  printf("Enter the base number:");
+  scanf("%d",&x);
+  printf("Enter the exponent number:");
+  scanf("%d",&y);
+  result =pow(x, y);
+  printf("%d is the result",result );
