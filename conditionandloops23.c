3a01dd98311a44837dae726484164528f9ba5953
Drishti Saini <drishti25csu349@ncuindia.edu>
2025-12-27 22:35:56 +0530

    Create conditionandloops23.c

---
+++ conditionandloops23.c
@@ -0,0 +1,17 @@
+ int a;
+    printf("your first number is:",a);
+    scanf("%d",&a);
+    int b;
+    printf("your second number is:",b);
+    scanf("%d",&b);
+    int sum= a+b;
+    printf("addition of %d and %d is %d\n",a,b,a+b);
+
+    int c;
+    printf("numbr for subtraction is:",c);
+    scanf("%d",&c);
+    int d;
+    printf("number for subtraction is:",d);
+    scanf("%d",&d);
+    int difference=c-d;
+    printf("subtraction of two numbers that is %d and %d is %d\n",c,d,c-d);
