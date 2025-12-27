744b1c5aba03f23661849bd75c3a3ed7cbc0f52f
Drishti Saini <drishti25csu349@ncuindia.edu>
2025-12-27 22:35:16 +0530

    Create conditionandloops22.c

---
+++ conditionandloops22.c
@@ -0,0 +1,13 @@
+
+ #include<stdio.h>
+int main(){
+    float base,  height,area;
+
+ printf(" Enter the base of the triangle:");
+ scanf("%f",&base);
+ printf("enter the height of the triangle:");
+ scanf("%f",&height);
+area = (0.5*base)*height;
+printf("you area of the triangle is:%f",area);
+
+    return 0;
